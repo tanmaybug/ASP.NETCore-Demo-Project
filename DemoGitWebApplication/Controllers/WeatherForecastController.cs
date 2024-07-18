53093b3176c24d772ba880a521bd24d0d082f6ee
Tanmay <38372175+tanmaybug@users.noreply.github.com>
2024-07-18 13:15:01 +0530

    Update WeatherForecastController.cs

--- DemoGitWebApplication/Controllers/WeatherForecastController.cs
+++ DemoGitWebApplication/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoGitWebApplication.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","XYZ"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
