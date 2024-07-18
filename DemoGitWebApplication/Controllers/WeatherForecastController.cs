5bbf44bf58f7066df279bccd54f41a9f730ece7a
tanmaybug <tanmay.bug@gmail.com>
2024-07-18 13:27:18 +0530

    test commit

--- DemoGitWebApplication/Controllers/WeatherForecastController.cs
+++ DemoGitWebApplication/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace DemoGitWebApplication.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var x;
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
